e04755bb3490a66096528b201995b246e83eeacc
McDonnough <psbaechli+atlassian@gmail.com>
2017-03-15 16:36:07 +0100

    formatted code according to checkstyle

--- src/main/java/io/teiler/api/endpoint/HelloWorld.java
+++ src/main/java/io/teiler/api/endpoint/HelloWorld.java
@@ -1,7 +1,7 @@
 package io.teiler.api.endpoint;
 
-import static spark.Spark.get;
 import static spark.Spark.before;
+import static spark.Spark.get;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,24 +11,24 @@ import org.springframework.stereotype.Component;
 public class HelloWorld {
 
     private static Logger LOGGER = LoggerFactory.getLogger(HelloWorld.class);
-    
+
     public HelloWorld() {
         register();
     }
-    
+
     public void register() {
         before((req, res) -> LOGGER.debug("API call to '" + req.pathInfo() + "'"));
         before((req, res) -> {
-           String path = req.pathInfo();
-           if (path.endsWith("/")) {
-               String newPath = path.substring(0, path.length() - 1);
-               LOGGER.debug("Call to '" + path + "' redirected to '" + newPath + "'");
-               res.redirect(newPath);
-           }
+            String path = req.pathInfo();
+            if (path.endsWith("/")) {
+                String newPath = path.substring(0, path.length() - 1);
+                LOGGER.debug("Call to '" + path + "' redirected to '" + newPath + "'");
+                res.redirect(newPath);
+            }
         });
-        
+
         get("/hello", (req, res) -> "Hello World");
         get("/hello/:name", (req, res) -> "Hello " + req.params(":name"));
     }
-    
+
 }
--- src/main/java/io/teiler/server/config/SparkConfiguration.java
+++ src/main/java/io/teiler/server/config/SparkConfiguration.java
@@ -8,10 +8,10 @@ import io.teiler.api.endpoint.HelloWorld;
 
 @Configuration
 public class SparkConfiguration {
-    
+
     @Bean
     CommandLineRunner sparkRunner() {
         return args -> new HelloWorld();
     }
-    
+
 }
