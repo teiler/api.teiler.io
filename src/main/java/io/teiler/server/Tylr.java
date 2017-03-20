bb2c5a795a9af3e3b8a9d78a3d5fa4be20375732
McDonnough <psbaechli+atlassian@gmail.com>
2017-03-20 13:45:12 +0100

    add component-scan to pick up spring-stuff

--- src/main/java/io/teiler/server/Tylr.java
+++ src/main/java/io/teiler/server/Tylr.java
@@ -2,8 +2,10 @@ package io.teiler.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("io.teiler")
 public class Tylr {
 
     public static void main(String[] args) {
