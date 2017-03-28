f3e2eed65a0530393a8d18c6822047f649f0c575
McDonnough <psbaechli+atlassian@gmail.com>
2017-03-28 13:32:38 +0200

    add test for group-endpoint

---
+++ src/test/java/io/teiler/api/endpoint/GroupEndpointTest.java
@@ -0,0 +1,34 @@
+package io.teiler.api.endpoint;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.teiler.server.Tylr;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Tylr.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(properties = {"local.server.port=4567"})
+public class GroupEndpointTest {
+    
+    @LocalServerPort
+    private int port;
+    
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+    
+    @Test
+    public void shouldReturn401WhenViewingGroupWithoutValidUUID() {
+        ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:" + port + "/v1/group", String.class);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    }
+
+}
