381663a437ef84e685c40336e0c91163d79a6ca3
Lukas Röllin <lsroellin@gmail.com>
2017-03-28 09:50:34 +0200

    Make Group ID unique

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -1,6 +1,6 @@
 /* do this with user tylr */
 
 CREATE TABLE IF NOT EXISTS "group" (
-    uuid character varying(50) NOT NULL,
+    uuid character varying(50) NOT NULL UNIQUE,
     name character varying(50) NOT NULL
 );
