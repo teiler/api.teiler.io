76b819d31aca6cfb2bafcd5113d60d7294bae97e
Lukas Röllin <lsroellin@gmail.com>
2017-03-29 10:12:23 +0200

    Add unit tests

--- src/test/java/io/teiler/api/endpoint/GroupEndpointTest.java
+++ src/test/java/io/teiler/api/endpoint/GroupEndpointTest.java
@@ -2,21 +2,30 @@ package io.teiler.api.endpoint;
 
 import com.google.gson.Gson;
 import io.teiler.api.endpoint.util.Util;
+import io.teiler.api.service.GroupService;
+import io.teiler.server.Tylr;
 import io.teiler.server.dto.Group;
+import io.teiler.server.util.exceptions.NotAuthorizedException;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = Tylr.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@TestPropertySource(properties = {"local.server.port=4567"})
-//@AutoConfigureTestDatabase
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Tylr.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(properties = {"local.server.port=4567"})
+@AutoConfigureTestDatabase
 public class GroupEndpointTest {
     
     @LocalServerPort
@@ -27,23 +36,17 @@ public class GroupEndpointTest {
     
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private GroupService groupService;
     
     @Autowired
     private Util util;
     
-//    @Test
-    public void shouldReturn401WhenViewingGroupWithoutValidUUID() {
-
-        ResponseEntity<String> response = testRestTemplate.getForEntity("http://" + ip + ":" + port + "/v1/group", String.class);
-        Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-    }
-    
-//    @Test
-    public void shouldReturn401WhenViewingGroupWithEmptyUUID() {
-        String url = "http://" + ip + ":" + util.getPort() + "/v1/group";
-        HttpEntity entity = Util.getHttpEntityWithHeader("");
-        ResponseEntity<String> response = testRestTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+    @Test(expected = NotAuthorizedException.class)
+    public void shouldReturnNotAuthorizedWhenViewingGroupWithoutValidUUID()
+        throws NotAuthorizedException {
+        groupService.viewGroup("");
     }
 
 //    @Test
