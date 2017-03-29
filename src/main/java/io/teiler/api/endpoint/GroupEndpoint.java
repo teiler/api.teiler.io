c38fc4b3bad6be0986f5e9a2cdc6e63c37d8af85
Lukas Röllin <lsroellin@gmail.com>
2017-03-29 08:53:11 +0200

    Make instance private, reorder lambda

--- src/main/java/io/teiler/api/endpoint/GroupEndpoint.java
+++ src/main/java/io/teiler/api/endpoint/GroupEndpoint.java
@@ -15,22 +15,15 @@ import org.springframework.stereotype.Controller;
 public class GroupEndpoint implements Endpoint {
     private static final Logger LOGGER = LoggerFactory.getLogger(GroupEndpoint.class);
 
-
     @Autowired
-    GroupService groupService;
+    private GroupService groupService;
 
     @Override
     public void register() {
         before((req, res) -> LOGGER.debug("API call to '" + req.pathInfo() + "'"));
 
-        post("/v1/group", (req, res) -> {
-            return groupService.handlePost(req);
-        });
+        post("/v1/group", (req, res) -> groupService.handlePost(req));
 
-        get("/v1/group", (req, res) -> {
-            return groupService.handleGet(req);
-        });
-
-
+        get("/v1/group", (req, res) -> groupService.handleGet(req));
     }
 }
