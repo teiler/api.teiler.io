d9a156adb3fe2a28a0ceda9bd3ef3957a890e28a
thde <d@thde.io>
2017-03-30 15:13:33 +0200

    updated names and defaults

--- src/main/resources/sql/tables.sql
+++ src/main/resources/sql/tables.sql
@@ -1,9 +1,9 @@
-CREATE TYPE currency AS ENUM (
+CREATE TYPE CURRENCY AS ENUM (
   'chf',
   'eur'
 );
 
-CREATE TYPE transaction_type AS ENUM (
+CREATE TYPE TRANSACTIONTYPE AS ENUM (
   'expense',
   'compensation'
 );
@@ -11,7 +11,9 @@ CREATE TYPE transaction_type AS ENUM (
 CREATE TABLE IF NOT EXISTS "group" (
   "id"  VARCHAR(50),
   "name"  VARCHAR(50) NOT NULL,
-  "currency"  currency NOT NULL,
+  "currency"  CURRENCY NOT NULL,
+  "create_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  "update_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -19,6 +21,8 @@ CREATE TABLE IF NOT EXISTS "person" (
   "id"  SERIAL,
   "name"  VARCHAR(50) NOT NULL,
   "group" VARCHAR(50) NOT NULL REFERENCES "group",
+  "create_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  "update_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -26,10 +30,10 @@ CREATE TABLE IF NOT EXISTS "transaction" (
   "id"  SERIAL,
   "title" VARCHAR(50) NOT NULL,
   "amount" INTEGER NOT NULL,
-  "createTime" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "updateTime" TIMESTAMP WITH TIME ZONE NOT NULL,
-  "type" transaction_type NOT NULL,
+  "type" TRANSACTIONTYPE NOT NULL,
   "payer" INTEGER NOT NULL REFERENCES "person",
+  "create_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  "update_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   PRIMARY KEY ("id")
 );
 
@@ -37,5 +41,7 @@ CREATE TABLE IF NOT EXISTS "profiteer" (
   "person" INTEGER REFERENCES "person",
   "transaction" INTEGER REFERENCES "transaction",
   "factor" DECIMAL(5) NOT NULL,
+  "create_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  "update_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   PRIMARY KEY ("person", "transaction")
 );
