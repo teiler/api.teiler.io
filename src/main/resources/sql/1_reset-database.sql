0e1d026853f8b446a6673aebb1d16b68d532c05f
Lukas Röllin <lsroellin@gmail.com>
2017-04-04 11:43:44 +0200

    Fix SQL

--- src/main/resources/sql/1_reset-database.sql
+++ src/main/resources/sql/1_reset-database.sql
@@ -5,9 +5,7 @@ psql postgres -f <file>
 
 SET client_min_messages = ERROR;
 \set user tylr
-\set password '\'tylr\''
 \set database tylr
 \set promptvar ''
 
 DROP DATABASE if exists :database;
-DROP USER if exists :user;
