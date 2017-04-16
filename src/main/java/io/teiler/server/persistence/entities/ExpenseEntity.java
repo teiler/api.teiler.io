4df310e7d390656f1774d618127f73bc891ed027
McDonnough <psbaechli+atlassian@gmail.com>
2017-04-16 22:43:44 +0200

    add expense entity (#173)

---
+++ src/main/java/io/teiler/server/persistence/entities/ExpenseEntity.java
@@ -0,0 +1,69 @@
+package io.teiler.server.persistence.entities;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import io.teiler.server.dto.Expense;
+import io.teiler.server.dto.TransactionType;
+import io.teiler.server.util.TimeUtil;
+
+/**
+ * Entity representing an Expense-entry of the <code>transaction</code>-table.
+ * 
+ * @author pbaechli
+ */
+@Entity
+@DiscriminatorValue("expense")
+public class ExpenseEntity extends TransactionEntity {
+
+    @NotNull
+    @Column(name = "title")
+    private String title;
+
+    @OneToMany(mappedBy = "person", fetch = FetchType.EAGER)
+    private List<ProfiteerEntity> profiteers;
+
+    public ExpenseEntity() { /* intentionally empty */ }
+
+    public ExpenseEntity(Expense expense) {
+        super.setId(expense.getId());
+        super.setAmount(expense.getAmount());
+        super.setPayer(new PersonEntity(expense.getPayer()));
+        super.setUpdateTime(TimeUtil.convertToTimestamp(expense.getUpdateTime()));
+        super.setCreateTime(TimeUtil.convertToTimestamp(expense.getCreateTime()));
+        super.setTransactionType(TransactionType.expense);
+
+        this.title = expense.getTitle();
+    }
+
+    public Expense toExpense() {
+        return new Expense(getId(), getAmount(), getPayer().toPerson(),
+                TimeUtil.convertToLocalDateTime(getUpdateTime()),
+                TimeUtil.convertToLocalDateTime(getCreateTime()), title,
+                profiteers.stream().map(p -> p.toShare()).collect(Collectors.toList()));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<ProfiteerEntity> getProfiteers() {
+        return profiteers;
+    }
+
+    public void setProfiteers(List<ProfiteerEntity> profiteers) {
+        this.profiteers = profiteers;
+    }
+
+}
