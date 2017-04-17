9ea21580aa81332b0d9d13ac114acb39977ca5f0
McDonnough <psbaechli+atlassian@gmail.com>
2017-04-17 13:45:46 +0200

    fix column definition of profteer.factor (#173)

--- src/dist/sql/create_tables.sql
+++ src/dist/sql/create_tables.sql
@@ -41,7 +41,7 @@ CREATE TABLE "profiteer" (
   "id"  SERIAL,
   "person" INTEGER REFERENCES "person" ON DELETE CASCADE,
   "transaction" INTEGER REFERENCES "transaction",
-  "factor" DECIMAL(5) NOT NULL,
+  "factor" DECIMAL(5, 4) NOT NULL,
   "create_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
   "update_time" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
   PRIMARY KEY ("person", "transaction")
