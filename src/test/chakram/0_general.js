8cb0d3f06ecf884321d49de1a5920d72e58a7e54
thde <d@thde.io>
2017-04-25 11:21:51 +0200

    add basic endpoint test

---
+++ src/test/chakram/0_general.js
@@ -0,0 +1,11 @@
+var chakram = require('chakram'),
+    expect = chakram.expect;
+
+describe("Teiler.io Root URL", function() {
+    it("should provide a simple json with the doc url", function () {
+        var response = chakram.get("https://web.teiler.io");
+        expect(response).to.have.status(200);
+	expect(response).to.have.header("content-type", "application/json");
+        return chakram.wait();
+    });
+});
