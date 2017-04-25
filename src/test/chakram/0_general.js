86166904512451aad73c34123df2b258e81e17db
thde <d@thde.io>
2017-04-25 11:42:18 +0200

    fix url

--- src/test/chakram/0_general.js
+++ src/test/chakram/0_general.js
@@ -3,7 +3,7 @@ var chakram = require('chakram'),
 
 describe("Teiler.io Root URL", function() {
     it("should provide a simple json with the doc url", function () {
-        var response = chakram.get("https://web.teiler.io");
+        var response = chakram.get("https://api.teiler.io");
         expect(response).to.have.status(200);
 	expect(response).to.have.header("content-type", "application/json");
         return chakram.wait();
