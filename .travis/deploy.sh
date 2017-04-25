543d10946f029dc3ec39194cd91a9a8b3024c000
thde <d@thde.io>
2017-04-25 13:03:49 +0200

    add prod tests to deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,3 +13,7 @@ ssh tylr@api.teiler.io << EOF
  tar -xvf /home/tylr/tylr-api.tar -C /srv/http/tylr-api/  --overwrite --strip-components=1
  sudo /etc/init.d/tylr-api start
 EOF
+
+npm install --dev
+npm install -g mocha
+mocha src/test/chakram
