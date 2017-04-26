cdbf1b0f1ff80231d55145f003e1cda47298e1c0
Lukas Röllin <lsroellin@gmail.com>
2017-04-26 11:34:13 +0200

    Split into multiple files (one for each endpoint)

--- src/test/chakram/groups.js
+++ src/test/chakram/groups.js
@@ -6,18 +6,6 @@ var hostUrl = process.env.tylrurl;
 var version = "v1/";
 var baseUrl = hostUrl + version;
 
-describe("Root URL", function() {
-    it("should provide a simple json with the doc url", function () {
-        var response = chakram.get(hostUrl);
-        expect(response).to.have.status(200);
-	      expect(response).to.have.header("content-type", "application/json");
-        expect(response).to.have.json("documentation", function (url) {
-            expect(url).to.equal("doc.teiler.io");
-        });
-        return chakram.wait();
-    });
-});
-
 describe("Create a group", function () {
   it("should create group", function () {
     var group = {
