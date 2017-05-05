5e46b3dad18a22c04632b72eb98f4c77468e2669
thde <d@thde.io>
2017-05-06 01:38:27 +0200

    add query

--- src/main/java/io/teiler/server/persistence/repositories/DebtRepository.java
+++ src/main/java/io/teiler/server/persistence/repositories/DebtRepository.java
@@ -1,8 +1,6 @@
 package io.teiler.server.persistence.repositories;
 
-import com.querydsl.jpa.impl.JPAQuery;
-import io.teiler.server.dto.Debt;
-
+import io.teiler.server.persistence.entities.DebtEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -22,14 +20,40 @@ public class DebtRepository {
     private EntityManager entityManager;
 
     /**
-     * Returns a {@link List} of {@link Debt} in the Group with the given Id.
+     * Returns a {@link List} of {@link DebtEntity} in the Group with the given Id.
      *
      * @param groupId Id of the Group
-     * @return {@link List} of {@link Debt}
+     * @return {@link List} of {@link DebtEntity}
      */
-    public List<Debt> get(String groupId) {
-        Query q = entityManager.createNativeQuery("SELECT a.id, a.version, a.firstname, a.lastname FROM Author a", Debt.class);
-        
+    public List<DebtEntity> get(String groupId) {
+        Query query = entityManager.createNativeQuery(
+                "SELECT\n" +
+                        "    person.id AS person,\n" +
+                        "    transactions.credit - profiteers.debt AS balance\n" +
+                        "FROM person\n" +
+                        "LEFT JOIN (\n" +
+                        "    SELECT \n" +
+                        "        transaction.payer,\n" +
+                        "        SUM(profiteer.share) AS credit\n" +
+                        "    FROM transaction\n" +
+                        "    LEFT JOIN profiteer ON profiteer.transaction=transaction.id\n" +
+                        "    LEFT JOIN person ON person.id=profiteer.person\n" +
+                        "    WHERE person.\"group\"='1'\n" +
+                        "    GROUP BY transaction.payer\n" +
+                        ") AS transactions ON transactions.payer=person.id\n" +
+                        "LEFT JOIN (\n" +
+                        "    SELECT \n" +
+                        "        profiteer.person,\n" +
+                        "        SUM(profiteer.share) AS debt\n" +
+                        "    FROM profiteer\n" +
+                        "    LEFT JOIN person ON person.id=profiteer.person\n" +
+                        "    WHERE person.\"group\"='1'\n" +
+                        "    GROUP BY profiteer.person\n" +
+                        ") AS profiteers ON profiteers.person=person.id",
+                DebtEntity.class);
+
+        return (List<DebtEntity>) query.getResultList();
+
     }
 
 }
