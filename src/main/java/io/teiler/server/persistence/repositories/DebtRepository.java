3e504c69a29a839eb661ae9d33e73b65806bf6f8
thde <d@thde.io>
2017-05-06 16:16:02 +0200

    fix query format

--- src/main/java/io/teiler/server/persistence/repositories/DebtRepository.java
+++ src/main/java/io/teiler/server/persistence/repositories/DebtRepository.java
@@ -29,28 +29,28 @@ public class DebtRepository {
     public List<DebtEntity> get(String groupId) {
         String sql =
                 "SELECT" +
-                "    person.id AS person," +
-                "    transactions.credit - profiteers.debt AS balance" +
-                "FROM person" +
-                "LEFT JOIN (" +
-                "    SELECT " +
-                "        transaction.payer," +
-                "        SUM(profiteer.share) AS credit" +
+                "    person.id AS person, " +
+                "    transactions.credit - profiteers.debt AS balance " +
+                "FROM person " +
+                "LEFT JOIN ( " +
+                "    SELECT  " +
+                "        transaction.payer, " +
+                "        SUM(profiteer.share) AS credit " +
                 "    FROM transaction" +
-                "    LEFT JOIN profiteer ON profiteer.transaction=transaction.id" +
-                "    LEFT JOIN person ON person.id=profiteer.person" +
-                "    WHERE person.\"group\"=?1" +
-                "    GROUP BY transaction.payer" +
-                ") AS transactions ON transactions.payer=person.id" +
-                "LEFT JOIN (" +
-                "    SELECT " +
-                "        profiteer.person," +
-                "        SUM(profiteer.share) AS debt" +
-                "    FROM profiteer" +
-                "    LEFT JOIN person ON person.id=profiteer.person" +
-                "    WHERE person.\"group\"=?1" +
-                "    GROUP BY profiteer.person" +
-                ") AS profiteers ON profiteers.person=person.id";
+                "    LEFT JOIN profiteer ON profiteer.transaction=transaction.id " +
+                "    LEFT JOIN person ON person.id=profiteer.person " +
+                "    WHERE person.\"group\"=?1 " +
+                "    GROUP BY transaction.payer " +
+                ") AS transactions ON transactions.payer=person.id " +
+                "LEFT JOIN ( " +
+                "    SELECT  " +
+                "        profiteer.person, " +
+                "        SUM(profiteer.share) AS debt " +
+                "    FROM profiteer " +
+                "    LEFT JOIN person ON person.id=profiteer.person " +
+                "    WHERE person.\"group\"=?1 " +
+                "    GROUP BY profiteer.person " +
+                ") AS profiteers ON profiteers.person=person.id ";
 
         Query query = entityManager.createNativeQuery(sql, STATEMENT_SQLMAP);
         query.setParameter(1, groupId);
