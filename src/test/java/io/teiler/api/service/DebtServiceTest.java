1c1feec14cdd810712ce31b74fe4980c08a67cf7
thde <d@thde.io>
2017-05-06 14:45:26 +0200

    add first test

---
+++ src/test/java/io/teiler/api/service/DebtServiceTest.java
@@ -0,0 +1,45 @@
+package io.teiler.api.service;
+
+import io.teiler.server.Tylr;
+import io.teiler.server.dto.Group;
+import io.teiler.server.dto.Person;
+import io.teiler.server.services.DebtService;
+import io.teiler.server.services.GroupService;
+import io.teiler.server.services.PersonService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Tylr.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(properties = {"local.server.port=4567"})
+@AutoConfigureTestDatabase
+public class DebtServiceTest {
+
+    private static final String FIRST_PERSON_NAME = "Johannes";
+    private static final String SECOND_PERSON_NAME = "Rudolf";
+    private static final String THIRD_PERSON_NAME = "Sepp";
+    private static final String TEST_GROUP_NAME = "Testgroup";
+
+    @Autowired
+    private DebtService debtService;
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private GroupService groupService;
+
+    @Test
+    public void testReturnZeroBalanceWithOnePersonAndNoneTransactions() {
+        Group testGroup = groupService.createGroup(TEST_GROUP_NAME);
+        String groupId = testGroup.getId();
+        Person firstPerson = personService.createPerson(groupId, FIRST_PERSON_NAME);
+        debtService.getDebt(groupId);
+    }
+
+}
