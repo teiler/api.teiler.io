a6a2fa9f656a3794a9cf26bcd9e0bd63db477a4a
thde <d@thde.io>
2017-05-07 01:08:22 +0200

    add basic tests

--- src/test/java/io/teiler/api/service/DebtServiceTest.java
+++ src/test/java/io/teiler/api/service/DebtServiceTest.java
@@ -1,5 +1,8 @@
 package io.teiler.api.service;
 
+import io.teiler.server.dto.*;
+import io.teiler.server.services.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +12,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.teiler.server.Tylr;
-import io.teiler.server.dto.Group;
-import io.teiler.server.dto.Person;
-import io.teiler.server.services.DebtService;
-import io.teiler.server.services.GroupService;
-import io.teiler.server.services.PersonService;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Tylr.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -30,17 +31,57 @@ public class DebtServiceTest {
     private DebtService debtService;
 
     @Autowired
+    private ExpenseService expenseService;
+
+    @Autowired
+    private CompensationService compensationService;
+
+    @Autowired
     private PersonService personService;
 
     @Autowired
     private GroupService groupService;
 
     @Test
+    public void testReturnCorrectBalance() {
+        final Integer SHARE = 5;
+
+        Group testGroup = groupService.createGroup(TEST_GROUP_NAME);
+        String groupId = testGroup.getId();
+
+        Person firstPerson = personService.createPerson(groupId, FIRST_PERSON_NAME);
+        Person secondPerson = personService.createPerson(groupId, SECOND_PERSON_NAME);
+        Person thirdPerson = personService.createPerson(groupId, THIRD_PERSON_NAME);
+
+        List<Profiteer> firstExpenseProfiteers = new LinkedList<>();
+        firstExpenseProfiteers.add(new Profiteer(null, firstPerson, SHARE));
+        firstExpenseProfiteers.add(new Profiteer(null, secondPerson, SHARE));
+        firstExpenseProfiteers.add(new Profiteer(null, thirdPerson, SHARE));
+
+        Expense firstExpense = new Expense(null, 3 * SHARE, firstPerson, "Test", firstExpenseProfiteers);
+        expenseService.createExpense(firstExpense, groupId);
+
+        System.out.println(firstPerson.getId());
+        System.out.println(debtService.getDebt(groupId).toString());
+
+        for (Debt debt : debtService.getDebt(groupId)) {
+            if (debt.getPerson() == firstPerson.getId()) {
+                Assert.assertEquals(new Integer(10), debt.getBalance());
+            } else {
+                Assert.assertEquals(new Integer(-5), debt.getBalance());
+            }
+        }
+    }
+
+    @Test
     public void testReturnZeroBalanceWithOnePersonAndNoneTransactions() {
         Group testGroup = groupService.createGroup(TEST_GROUP_NAME);
         String groupId = testGroup.getId();
+
         Person firstPerson = personService.createPerson(groupId, FIRST_PERSON_NAME);
-        debtService.getDebt(groupId);
+
+        List<Debt> debts = debtService.getDebt(groupId);
+        Assert.assertEquals(new Integer(0), debts.get(0).getBalance());
     }
 
 }
