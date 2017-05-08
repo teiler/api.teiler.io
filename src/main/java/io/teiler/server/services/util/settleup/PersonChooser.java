f6622dc05032d471e32a78ce891bb9d4bdfd90e4
thde <d@thde.io>
2017-05-08 13:08:30 +0200

    remove unused imports

--- src/main/java/io/teiler/server/services/util/settleup/PersonChooser.java
+++ src/main/java/io/teiler/server/services/util/settleup/PersonChooser.java
@@ -1,9 +1,6 @@
 package io.teiler.server.services.util.settleup;
 
 import io.teiler.server.dto.Debt;
-import io.teiler.server.dto.Person;
-
-import java.util.List;
 
 /**
  * Provides methods to choose the next person to calculate suggested payments.
