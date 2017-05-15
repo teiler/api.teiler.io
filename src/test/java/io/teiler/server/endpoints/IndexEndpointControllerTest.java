a45b860bb642d84e912f3d31b82af20bd170b33d
McDonnough <psbaechli+atlassian@gmail.com>
2017-05-15 10:47:19 +0200

    disable index endpoint test

--- src/test/java/io/teiler/server/endpoints/IndexEndpointControllerTest.java
+++ src/test/java/io/teiler/server/endpoints/IndexEndpointControllerTest.java
@@ -1,12 +1,10 @@
 package io.teiler.server.endpoints;
 
-import static io.restassured.RestAssured.get;
-
 public class IndexEndpointControllerTest extends BaseEndpointControllerTest {
     
 //    @Test
-    public void testIndexEndpoint() {
-        get("").then().assertThat().statusCode(200);
-    }
+//    public void testIndexEndpoint() {
+//        get("").then().assertThat().statusCode(200);
+//    }
 
 }
