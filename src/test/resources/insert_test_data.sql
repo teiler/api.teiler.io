c0774f93279aedd41c47863223f90e3370248264
McDonnough <psbaechli+atlassian@gmail.com>
2017-05-17 09:50:56 +0200

    add test for people endpoints

--- src/test/resources/insert_test_data.sql
+++ src/test/resources/insert_test_data.sql
@@ -1,3 +1,12 @@
 INSERT INTO "group" VALUES ('gettgrup', 'GetGroup', 'CHF', NOW(), NOW());
 INSERT INTO "group" VALUES ('puttgrup', 'EditGroup', 'CHF', NOW(), NOW());
 INSERT INTO "group" VALUES ('deltgrup', 'DeleteGroup', 'CHF', NOW(), NOW());
+
+INSERT INTO "group" VALUES ('persgrup', 'PersonGroup', 'CHF', NOW(), NOW());
+INSERT INTO person VALUES (1, 'GetPerson', 'persgrup', TRUE, NOW(), NOW());
+INSERT INTO person VALUES (2, 'PutPerson', 'persgrup', TRUE, NOW(), NOW());
+INSERT INTO person VALUES (3, 'DeletePerson', 'persgrup', TRUE, NOW(), NOW());
+
+INSERT INTO "group" VALUES ('piplgrup', 'PeopleGroup', 'CHF', NOW(), NOW());
+INSERT INTO person VALUES (7, 'PeoplePerson1', 'piplgrup', TRUE, NOW(), NOW());
+INSERT INTO person VALUES (8, 'PeoplePerson2', 'piplgrup', TRUE, NOW(), NOW());
