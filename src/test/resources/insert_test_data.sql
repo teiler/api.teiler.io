67dadf9543c6e8fec5d3cd2cb8be985e90de8169
McDonnough <psbaechli+atlassian@gmail.com>
2017-05-22 11:04:09 +0200

    update person-sequence with max(id)

--- src/test/resources/insert_test_data.sql
+++ src/test/resources/insert_test_data.sql
@@ -1,7 +1,9 @@
+-- test-data for groups
 INSERT INTO "group" VALUES ('gettgrup', 'GetGroup', 'CHF', NOW(), NOW());
 INSERT INTO "group" VALUES ('puttgrup', 'EditGroup', 'CHF', NOW(), NOW());
 INSERT INTO "group" VALUES ('deltgrup', 'DeleteGroup', 'CHF', NOW(), NOW());
 
+-- test-data for people
 INSERT INTO "group" VALUES ('persgrup', 'PersonGroup', 'CHF', NOW(), NOW());
 INSERT INTO person VALUES (1, 'GetPerson', 'persgrup', TRUE, NOW(), NOW());
 INSERT INTO person VALUES (2, 'PutPerson', 'persgrup', TRUE, NOW(), NOW());
@@ -10,3 +12,5 @@ INSERT INTO person VALUES (3, 'DeletePerson', 'persgrup', TRUE, NOW(), NOW());
 INSERT INTO "group" VALUES ('piplgrup', 'PeopleGroup', 'CHF', NOW(), NOW());
 INSERT INTO person VALUES (7, 'PeoplePerson1', 'piplgrup', TRUE, NOW(), NOW());
 INSERT INTO person VALUES (8, 'PeoplePerson2', 'piplgrup', TRUE, NOW(), NOW());
+
+SELECT SETVAL('person_id_seq', (SELECT MAX(id) FROM "person")); -- DO NOT REMOVE, ELSE: TRAVIS => BUMM
