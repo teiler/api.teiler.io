078d63cca5b943b777a0473e8c639bd2ad1b59d6
McDonnough <psbaechli+atlassian@gmail.com>
2017-05-22 17:24:40 +0200

    remove Chakram

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,9 +14,3 @@ ssh tylr@api.teiler.io << EOF
  # /srv/http/tylr-api/sql/setup-tables.sh
  sudo /etc/init.d/tylr-api start
 EOF
-
-while [ ! $(curl -s -I -f https://api.teiler.io | grep "200 OK" &> /dev/null) ]; do
-    printf '.'
-    sleep 1
-done
-./.travis/test-prod.sh
